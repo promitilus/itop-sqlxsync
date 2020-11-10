a0477d53f831c7183277c25fe26ec2acf3710bee
SamB <samuel.behan@dob.sk>
2020-11-10 13:46:51 +0100

    fix class name

--- collectors/SQLXHardDiskCollector.class.inc.php
+++ collectors/SQLXHardDiskCollector.class.inc.php
@@ -6,7 +6,7 @@
  * @author Sam <samuel.behan@dob.sk>
  * @license http://opensource.org/licenses/AGPL-3.0
  */
-class SQLXHardDisk extends SQLXCollector {
+class SQLXHardDiskCollector extends SQLXCollector {
 
 	public function __construct() {
 		return parent::__construct();
